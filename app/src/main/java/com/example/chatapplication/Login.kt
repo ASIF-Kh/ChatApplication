2041c59c0ba110fffcdad72e9982006ccd77be04
asif <asifiqbalk99@gmail.com>
2023-03-23 00:36:24 +0530

    Update1

---
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -0,0 +1,48 @@
+package com.example.chatapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
+class Login : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var btnsignup: Button
+
+    private lateinit var mAuth:FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth=FirebaseAuth.getInstance()
+
+        edtEmail=findViewById(R.id.edt_email)
+        edtPassword=findViewById(R.id.edt_password)
+        btnlogin=findViewById(R.id.login)
+        btnsignup=findViewById(R.id.signup)
+
+        btnsignup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnlogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password)
+        }
+    }
+
+    private fun login(email: String, password: String){
+        
+    }
+}
